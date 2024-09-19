6ecee52c8bfa14c93f7d241ca6ba9225ef6b3adf
kblatnik <kevin-blatnik@users.noreply.github.com>
2024-09-19 10:24:33 -0700

    try row divs

--- javascript.js
+++ javascript.js
@@ -1,23 +1,36 @@
-const rows = 16;
-const cols = 16;
-
 
 const etchSketch = {
     containerRef : document.getElementById("container"),
+    buttonRef : document.getElementById("resizeButton"),
+    inputRef : document.getElementById("sizeInput"),
+    rows : 16,
+    cols : 16,
 
-    addGrid : function() {
+    addGrid : function(rows,cols) {
         let squareDiv = null;
+        
+        let maxWindowHeight = window.innerHeight;
 
-        //Empty Container first?
+        //Empty Container first
+        this.emptyContainer();
 
         for(let i=0; i<rows; i++) {
+            
             for(let j=0; j<cols; j++) {
                 squareDiv = document.createElement("div");   
                 squareDiv.id = 'gridBox' + i + "-"+ j; 
                 squareDiv.row = i;
                 squareDiv.col = j;
-                squareDiv.style.width = '6vh';
-                squareDiv.style.height = '6vh'; //to make square
+
+                squareDiv.style.flexShrink = "0";
+                squareDiv.style.flexGrow = "1";
+                
+                squareDiv.style.width = Math.floor(maxWindowHeight/rows) + 'px';
+                squareDiv.style.height = Math.floor(maxWindowHeight/cols) + 'px'; //to make square
+
+                this.containerRef.style.width = squareDiv.style.width * rows;
+                this.containerRef.style.height = squareDiv.style.height * cols;
+
                 //squareDiv.style.border = '4em solid gray'; 
                 squareDiv.style.backgroundColor = "gray";
                 squareDiv.innerText = '.';
@@ -29,10 +42,10 @@ const etchSketch = {
     },
     addHover : function() {
         this.containerRef.addEventListener('mouseover', ev => {
-            console.log(ev.target);
+            //console.log(ev.target);
             const targeted = document.getElementById(ev.target.id);
             
-            console.log(targeted);
+            //console.log(targeted);
             if ( targeted !== null ) {
                 targeted.style.backgroundColor = "black";
             }
@@ -42,10 +55,22 @@ const etchSketch = {
         while(this.containerRef.hasChildNodes()) {
             this.containerRef.removeChild(this.containerRef.lastChild);
         }
-        //Have to remove listener and readd?
+    },
+    addResizeListener : function() {
+        this.buttonRef.addEventListener('click', ev => {
+            let size = Number(this.inputRef.value);
+
+            if(typeof size === 'number' && size >= 1 && size < 101) {
+                console.log("yes");
+                size = Math.floor(size);
+                this.addGrid(size,size);
+            }
+        });
     }
+
 }
 
-etchSketch.addGrid();
+//main
+etchSketch.addGrid(16,16);
 etchSketch.addHover();
-etchSketch.emptyContainer();
+etchSketch.addResizeListener();
